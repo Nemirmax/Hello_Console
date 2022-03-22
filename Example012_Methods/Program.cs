b2aa37c438666f36b254a87dd609dbdd07e4c209
Maxim <maxim.nemer@mail.ru>
2022-03-22 15:50:04 +0300

    Add example012

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,86 @@
+﻿// Вид 1
+// void Method1()
+//     {
+//         Console.WriteLine("Автор Немировский...");
+//     }
+//     Method1();
+    //Вид 2
+    // void Method2(string msg)
+    // {
+    //     Console.WriteLine(msg);
+    // }
+    // Method2(msg: "Текст сообщения");
+
+    // void Method21(string msg, int count)
+    // {
+    //     int i = 0;
+    //     while (i<count)
+    //     {
+    //         Console.WriteLine(msg);
+    //         i++;
+    //     }
+    // }
+    // Method21("Текст", count: 4);
+    // Method21(count: 4, msg: "новый текст");
+
+// Вид 3
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+//Вид 4
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i <count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.Write(res);
+// Второй вариант: 
+// string Method4(int count, string text)
+// {
+   
+//     string result = String.Empty;
+//     for( int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+//Console.WriteLine(res);
+
+//Например таблица умножения:
+// for (int i = 0; i <= 10; i++)
+// {
+//     for( int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+// =======Работа с текстом
+// Дан такст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими буквами "К",
+// а большие "С" заменить маленькими "с"
+string text ="-Я думаю, - сказал князь, улыбаясь, -что, "
++ "ежели вы нас послали вместо нашего милого Винценгероде, "
++ "Вы бы взяли приступом согласие прусского короля ."
++ "Вы так красноречивы. Вы дадите мне чаю? ";
+// string s= "qwerty"
+//            012
+// s[3]  // r
+string Replase(string text, char oldValue
